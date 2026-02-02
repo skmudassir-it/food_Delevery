6a9fc0846e14458c94aae002ce6ef16b746277fe
Mudassir Shaik <partner_1770006799@example.com>
2026-02-02 00:33:55 -0600

    Refactor logout to GET

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,10 +4,10 @@ const authController = require('../controllers/auth.controller');
 
 router.post('/user/register', authController.registerUser);
 router.post('/user/login', authController.loginUser);
-router.post('/user/logout', authController.logoutUser);
+router.get('/user/logout', authController.logoutUser);
 
 router.post('/foodpartner/register', authController.registerFoodPartner);
 router.post('/foodpartner/login', authController.loginFoodPartner);
-router.post('/foodpartner/logout', authController.logoutFoodPartner);
+router.get('/foodpartner/logout', authController.logoutFoodPartner);
 
 module.exports = router;
